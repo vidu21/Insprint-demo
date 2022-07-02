04e4c0c14fb286e72762cb3243fade3c65f0bf57
vidu21 <53952298+vidu21@users.noreply.github.com>
2022-07-02 15:42:37 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+  if(isset( $_POST['name']))
+  $name = $_POST['name'];
+  if(isset( $_POST['email']))
+  $email = $_POST['email'];
+  if(isset( $_POST['message']))
+  $message = $_POST['message'];
+  if(isset( $_POST['contact']))
+  $contact = $_POST['contact'];
+  $subject="New Contact Submission"
+  $content="From: $name \n Email: $email \n Contact Number: $contact \n Message: $message";
+  $recipient = "vidu.s1999@gmail.com";
+  $mailheader = "From: $email \r\n";
+  mail($recipient, $subject, $content, $mailheader) or die("Error!");
+  header('Location: thanks.html');
+  exit;
+?>
