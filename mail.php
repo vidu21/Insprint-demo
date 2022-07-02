5c919b367f56dc1d6edd6e6f30ce8f8418d92464
vidu21 <53952298+vidu21@users.noreply.github.com>
2022-07-02 17:25:42 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,4 +1,19 @@
 <?php
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = "test@hostinger-tutorials.com";
+$to = "vidu.s1999@gmail.com";
+$subject = "Checking PHP mail";
+$message = "PHP mail works just fine";
+$headers = "From:" . $from;
+if(mail($to,$subject,$message, $headers)) {
+    echo "The email message was sent.";
+} else {
+    echo "The email message was not sent.";
+}
+?>
+
+<!-- <?php
   if(isset( $_POST['name']))
   $name = $_POST['name'];
   if(isset( $_POST['email']))
@@ -17,4 +32,4 @@ if (!mail($to_email, $subject, $message, $headers)) {
   }
   header('Location: thanks.html');
   exit;
-?>
+?> -->
